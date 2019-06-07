7d320c7be2269e875d7fd05515538d4114cbb308
mayzinchit <mayzinchit@lfuturedev.com>
2019-06-07 17:02:27 +0630

    Uptodate Fix

--- mzc.js
+++ mzc.js
@@ -2,4 +2,4 @@ console.log("I'm MZC1 by TRAA");
 console.log("I'm MZC2 Hello");
 console.log("I'm MZC3");
 console.log("I'm MZC4 modified from branch ...klj");
-console.log("I'm MZC5");
+console.log("I'm MZC5 uptodate fix");
