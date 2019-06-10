9241e9770537f6ca3eb4a141215651be93f34443
Thu Rein Aung Aung <thureinaungaung810@gmail.com>
2019-06-10 12:34:36 +0630

    Today at 11:20 AM

--- traa.js
+++ traa.js
@@ -3,4 +3,4 @@ console.log("Hello Branch");
 console.log("Third Console");
 console.log("Fourth Console");
 console.log("Fifth Console");
-console.log("The Last Console today at 11:20 am");
+console.log("The Last Console today at 11:20 AM");
