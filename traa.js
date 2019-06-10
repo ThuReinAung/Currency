ea0049722e4f71605eecd7712653e8bede10709d
Thu Rein Aung Aung <thureinaungaung810@gmail.com>
2019-06-10 10:26:50 +0630

    Hello Branch Second

--- traa.js
+++ traa.js
@@ -1,6 +1,5 @@
 console.log("First Console");
-console.log("Second Console");
 console.log("Hello Branch");
+console.log("Third Console");
 console.log("Fourth Console");
-console.log("Fifth Console");
-console.log("The Last Console");
+console.log("Fifth Console");console.log("The Last Console");
