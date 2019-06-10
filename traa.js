a7afef0137b11f3fe7573723ef46643ec4eee583
Thu Rein Aung Aung <thureinaungaung810@gmail.com>
2019-06-10 10:59:37 +0630

    The Last Console

--- traa.js
+++ traa.js
@@ -1,5 +1,6 @@
-console.log("Hello Branch");
+console.log("First Console");
 console.log("Second Console");
 console.log("Third Console");
 console.log("Fourth Console");
 console.log("Fifth Console");
+console.log("The Last Console");
